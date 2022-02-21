bc64bd6c77fe4b10206ec1db9df8e53e783c6530
slsmphys <slsmphys@gmail.com>
2022-02-21 20:16:43 +0000

    add default global css file and various loaders to webpack

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./index.css"; // Global CSS file
+
 const App = () => <h1>Hello React!</h1>;
 
 ReactDOM.render(<App />, document.getElementById("root"));
